690fece63dce940cb3dc0ccffbcca07821a4fcb1
Artem Barafanov <st117258@student.spbu.ru>
2024-09-16 02:18:58 +0300

    Setting the starting position

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include "mainwindow.h"
 
-#include <QApplication>
+#include <QtWidgets>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QLabel *tic = new QLabel("Крестики-нолики",&w);
+    tic->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
+    tic->move(15,30);
+    QFont font = tic->font();
+    font.setPointSize(30);
+    tic->setFont(font);
+    tic->setFixedSize(550, 500);
+    tic->setAutoFillBackground(true);
+
+    QPushButton *game = new QPushButton("Игра",&w);
+    game->setEnabled(false);
+    game->setStyleSheet("color: grey");
+    font = game->font();
+    font.setPointSize(20);
+    game->setFont(font);
+    game->setFixedSize(200, 50);
+    game->move(80, 100);
+    game->setAutoFillBackground(true);
+
+    QPushButton *authorization = new QPushButton("Авторизация",&w);
+    font = authorization->font();
+    font.setPointSize(20);
+    authorization->setFont(font);
+    authorization->setFixedSize(200, 50);
+    authorization->move(300, 100);
+    authorization->setAutoFillBackground(true);
+
+    w.resize(600, 500);
     w.show();
     return a.exec();
 }
